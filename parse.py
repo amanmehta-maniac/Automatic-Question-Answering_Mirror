161048f7e8df7d473040fd83f83507898d209227
amanmehta-maniac <amanmehta1997@gmail.com>
2018-02-19 00:47:27 +0530

    coref updates

--- parse.py
+++ parse.py
@@ -1,4 +1,4 @@
-import re
+import re,sys
 
 P = ["He", "he", "Him", "him", "She", "she", "Her","her", "It", "it", "They", "they"]
 R = ["Himself", "himself", "Herself", "herself","Itself", "itself", "Themselves", "themselves"]
@@ -11,23 +11,47 @@ r_indxs = []
 words = read_words("test")
 abhi_tak = ""
 
-with open("test") as f:
-    text = f.read()
+chapNum = (int)(sys.argv[1])
+classIdentifier = "i"
 
-lines = re.split(r' *[\.\?!][\'"\)\]]* *', text)
+if chapNum > 8:
+	chapNum -= 8
+	classIdentifier = "j"
+
+file = open("./Dataset_NCERT/Dataset-txt/"+classIdentifier+"ess30"+str(chapNum)+".txt")
+sentences = file.read()
+file.close()
+sentences = re.split(r' *[\.\?!][\'"\)\]]* *', sentences)
+for i, s in enumerate(sentences):
+	s = s.replace("\n", " ")
+	# print POS
+	sentences[i] = s
+
+lines = sentences
+# lines = re.split(r' *[\.\?!][\'"\)\]]* *', text)
 spans = []
+pros = []
+start = 0
+end = 0
 
-for line in lines:
-	abhi_tak += line+". "
+for i,line in enumerate(lines):
+	abhi_tak = ""
+	for ind in range(start,i+1):
+		abhi_tak += lines[ind] + ". "
 	w_line = line.split(" ") 
 	for p in P:
 		if p in w_line:
 			spans.append(abhi_tak)
+			pros.append(p)
+	start = max(start,i-5);
 print spans
 
 for ind,i in enumerate(spans):
 	f = open("spans/"+str(ind)+".txt","w")
 	f.write(i)
+	f = open("pros/"+str(ind)+".txt","w")
+	f.write(pros[ind])
+
 
 for indx,word in enumerate(words):
 	# print indx,word
