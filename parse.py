eebb18f236f1bad86561ea6d227fe98b1b99b92e
Aashay <aashay225@gmail.com>
2018-02-18 20:41:28 +0530

    updated

--- parse.py
+++ parse.py
@@ -1,9 +1,7 @@
-# f = open("test","r")
-# lines = f.readlines()
+import re
 
-# print lines
-p = ["He", "he", "Him", "him", "She", "she", "Her","her", "It", "it", "They", "they"]
-r = ["Himself", "himself", "Herself", "herself","Itself", "itself", "Themselves", "themselves"]
+P = ["He", "he", "Him", "him", "She", "she", "Her","her", "It", "it", "They", "they"]
+R = ["Himself", "himself", "Herself", "herself","Itself", "itself", "Themselves", "themselves"]
 
 def read_words(words_file):
     return [word for line in open(words_file, 'r') for word in line.split()]
@@ -11,11 +9,32 @@ def read_words(words_file):
 p_indxs = []
 r_indxs = []
 words = read_words("test")
+abhi_tak = ""
+
+with open("test") as f:
+    text = f.read()
+
+lines = re.split(r' *[\.\?!][\'"\)\]]* *', text)
+spans = []
+
+for line in lines:
+	abhi_tak += line+". "
+	w_line = line.split(" ") 
+	for p in P:
+		if p in w_line:
+			spans.append(abhi_tak)
+print spans
+
+for ind,i in enumerate(spans):
+	f = open("spans/"+str(ind)+".txt","w")
+	f.write(i)
+
 for indx,word in enumerate(words):
-	print indx,word
-	if word in p:
+	# print indx,word
+	if word in P:
 		p_indxs.append(indx)
-	if word in r:
+
+	if word in R:
 		r_indxs.append(indx)
 
 print p_indxs,r_indxs
--- remove_newlines.py
+++ remove_newlines.py
@@ -1,4 +1,6 @@
-f = open("tempout.txt","r")
+import sys
+
+f = open(sys.argv[1],"r")
 lines = f.readlines()
 
 
