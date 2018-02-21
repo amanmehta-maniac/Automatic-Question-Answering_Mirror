5142fad37e5234481512bd4370d5bafed0b69494
aashay201297 <aashay225@gmail.com>
2018-02-21 23:15:32 +0530

    multiprocessing

--- parse_train.py
+++ parse_train.py
@@ -1,10 +1,10 @@
 import sys
-sys.path.append('./LCS')
-sys.path.append('./headRelated')
-sys.path.append('./ngramOverlap')
-sys.path.append('./skipBigram')
-sys.path.append('./synHypOverlap')
-sys.path.append('./treeKernel')
+sys.path.append('./Features/LCS')
+sys.path.append('./Features/headRelated')
+sys.path.append('./Features/ngramOverlap')
+sys.path.append('./Features/skipBigram')
+sys.path.append('./Features/synHypOverlap')
+sys.path.append('./Features/treeKernel')
 import os
 import re
 import pickle
@@ -50,7 +50,7 @@ print type(whip)
 # print type(whip[0])
 # for k in whip[0]:
 # 	print k
-print whip[0]['paragraphs'][0]
+#print whip[0]['paragraphs'][0]
 # print whip[0]['title']
 paras = whip[0]['paragraphs']
 train_x = []
@@ -59,6 +59,8 @@ train = []
 k = 0	
 x = 0
 num_calls = 0
+qid=0
+write_str = ""
 for index in range(len(whip)):
 	paras = whip[index]['paragraphs']
 	for para in paras:
@@ -74,28 +76,33 @@ for index in range(len(whip)):
 		c = c.split('.')
 		# print c
 		for q in para['qas']:
+			qid+=1 
 			for ci in c:
 				if ci != "":
 					num_calls += 1
 					# train.append([q['question'],ci,q['answers'][0]['text']])
 					############### isko comment karke ek file bana jisme question ids ho bas##############
-					feat = extract_features(q['question'],ci)
-					if q['answers'][0]['text'] in ci:
-						train_x.append(feat)
-						train_y.append(1)
-					else:
-						train_x.append(feat)
-						train_y.append(0)
+					#feat = extract_features(q['question'],ci)
+					feat_final = [qid]+[q['question'],ci]
+					write_str += str(qid)+","+q['question']+","+ci+"\n"
+					#if q['answers'][0]['text'] in ci:
+					#	train_x.append(feat_final)
+					#	train_y.append(1)
+					#else:
+					#	train_x.append(feat_final)
+					#	train_y.append(0)
 					# print "done for",k,q['question'],ci,q['answers'][0]['text']
 					k+=1
 			# break
-		f = open("features3/"+str(x)+".csv","w")
+		#f = open("features/"+str(x)+".csv","w")
+		f = open("question_list.csv","w")
 		final = ""
 		for i in range(len(train_x)):
 			for j in train_x[i]:
 				final +=  str(j)+","
 			final += str(train_y[i])+"\n"
 		print final
-		f.write(final)
+		f.write(write_str)
 		print "done writing for ",x
+		break
 print num_calls
